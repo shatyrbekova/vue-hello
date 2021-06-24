7833e6a5ef09152f9fb58c15fc7d63d7a902a562
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-24 21:42:07 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 
  //*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
- const app = new Vue(
+ const app = new Vue( //!Dichiarare Vue;
      {
-         el: '#app',
+         el: '#app', //! Dichiarare dentro quale container Vue verrà utilizzato;
          data: {
              msg: 'Hello world!',
+             name:'',
              sitename: 'Vue.js ',
              //*Aggiungere alla pagina un’immagine, presa anch’essa da un data
-             img: './img/vue_js.jpeg'
+             img: './img/vue_js.jpeg',
+             message:  new Date().toLocaleString()
          }
      }
- )
+ );
 
- 
+ new Vue ({
+     el:'#container',
+     data: {
+        
+     }
+ })
