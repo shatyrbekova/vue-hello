44da66dc48ff6f8d5ecda665c5047de18cccd484
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-24 14:10:42 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+ //*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+ const app = new Vue(
+     {
+         el: '#app',
+         data: {
+             msg: 'Hello world!',
+             img: './img/image1.jpg'
+         }
+     }
+ )
+
+ //*Aggiungere alla pagina un’immagine, presa anch’essa da un data
